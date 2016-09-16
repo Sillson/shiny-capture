1cdfefee3d6c137452d3be880aba7d916297c8b0
stuart <sillson88@gmail.com>
2016-09-16 02:22:44 -0700

    up

--- server.R
+++ server.R
@@ -21,9 +21,26 @@ shinyServer(function(input, output) {
     
     damData <- filter(runData, dam_id == input$damSelection)
     damData <- filter(damData, count_date > input$dateRange[1] & count_date < input$dateRange[2])
-
-    ggplot(damData, aes(count_date)) +
-      geom_line(data=damData[damData$fish_id==strtoi(input$salmonCheck), ], aes(y = count))
+    damData <- filter(damData, fish_id %in% input$salmonCheck)
+    
+    damData$fish_id[damData$fish_id == 1] <- "Chinook Adult"
+    damData$fish_id[damData$fish_id == 2] <- "Chinook Jack"
+    damData$fish_id[damData$fish_id == 3] <- "Steelhead"
+    damData$fish_id[damData$fish_id == 4] <- "Wild Steelhead"
+    damData$fish_id[damData$fish_id == 5] <- "Shad"
+    damData$fish_id[damData$fish_id == 6] <- "Sockeye"
+    damData$fish_id[damData$fish_id == 7] <- "Lamprey"
+    damData$fish_id[damData$fish_id == 8] <- "Coho Adult"
+    damData$fish_id[damData$fish_id == 9] <- "Coho Jack"
+    
+    damTable <- data.table(damData)
+    
+ 
+    
+    aggrSet <- damTable[, .(count=sum(count)), by=.(count_date, fish_id)]
+    
+    ggplot(data=aggrSet, aes(x=count_date, y=count, group=fish_id, colour=fish_id)) + 
+      geom_line()
 
   })
 
